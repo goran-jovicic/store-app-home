5be6ff83e1e6f4e10714faf4685afdee9ce4bcff
Goran Jovicic <gugacns94@gmail.com>
2019-07-26 21:34:46 +0200

    zadatak8

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -3,38 +3,50 @@
     <ul class="list-group">
       <li class="list-group-item" v-for="(product,id) in filteredProducts" :key="id">
         {{ `${product.name} Quantity : ${product.quantity}` }}
+        <button
+          class="btn btn-outline-primary btn-sm"
+          @click="incrementProduct(product)"
+        >+</button>
+        <button 
+          class="btn btn-outline-danger btn-sm" 
+          @click="decrementProduct(product)"
+        >-</button>
       </li>
     </ul>
-    <input type="text" placeholder="Search.." v-model="term">
+    <input type="text" placeholder="Search.." v-model="term" />
   </div>
 </template>
 
 <script>
-import { productService } from '../services/ProductService'
+import { productService } from "../services/ProductService";
 
 export default {
   data() {
     return {
-      products : productService.list(),
-      term : ''
+      products: productService.list(),
+      term: ""
     }
   },
 
-  computed : {
+  methods : {
+    incrementProduct(product) {
+      productService.increment(product)
+    },
+
+    decrementProduct(product) {
+      productService.decrement(product)
+    }
+  },
+
+  computed: {
     filteredProducts() {
       return this.products.filter(product => {
-        return product.name.toLowerCase().includes(this.term.toLowerCase())
-      })
+        return product.name.toLowerCase().includes(this.term.toLowerCase());
+      });
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
-
-
-// Postaviti niz proizvoda u `data()` komponente `AppProducts`
-// Implementirati listanje proizvoda u AppProducts komponenti
-// Komitovati urađeno
