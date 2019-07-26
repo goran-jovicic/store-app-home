59e38fdf01636a63b82c69fb35efc3265b8dd6cc
Goran Jovicic <gugacns94@gmail.com>
2019-07-26 20:35:41 +0200

    zadatak2

---
+++ src/components/AppCustomers.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item" 
+          v-for="(customer,index) in customers" 
+          :key="index"> 
+          {{ customer.name }} 
+          <button class="btn btn-outline-primary btn-sm" @click="removeCustomer(index)">Delete customer</button>
+          </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      customers : [
+        { name: 'Goran Jovicic' },
+        { name: 'Marko Trivunic' },
+        { name: 'Ivan Vujovic' }
+      ]
+    }
+  },
+
+  methods : {
+    removeCustomer(index) {
+      this.customers.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
