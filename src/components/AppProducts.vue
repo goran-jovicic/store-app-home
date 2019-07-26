5b05800f107e3ea53b81229816e600d9d95ef85d
Goran Jovicic <gugacns94@gmail.com>
2019-07-26 21:27:50 +0200

    zadatak 5

--- src/components/AppProducts.vue
+++ src/components/AppProducts.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="(product,id) in products" :key="id">
+        {{ `${product.name} Quantity : ${product.quantity}` }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
+import { productService } from '../services/ProductService'
 
+export default {
+  data() {
+    return {
+      products : productService.list()
+    }
+  }
 }
 </script>
 
 <style>
 
 </style>
+
+
+// Postaviti niz proizvoda u `data()` komponente `AppProducts`
+// Implementirati listanje proizvoda u AppProducts komponenti
+// Komitovati urađeno
