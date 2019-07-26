c0130fd96768a3abd7ba3bec8c5d4ea5e2c6d781
Goran Jovicic <gugacns94@gmail.com>
2019-07-26 20:46:12 +0200

    zadatak 3

--- src/components/AppCustomers.vue
+++ src/components/AppCustomers.vue
@@ -2,12 +2,26 @@
     <div>
       <ul class="list-group">
         <li class="list-group-item" 
-          v-for="(customer,index) in customers" 
-          :key="index"> 
-          {{ customer.name }} 
-          <button class="btn btn-outline-primary btn-sm" @click="removeCustomer(index)">Delete customer</button>
-          </li>
+          v-for="(customer,id) in customers" 
+          :key="id"> 
+          {{ `${customer.name} ${customer.email}` }} 
+          <button class="btn btn-outline-primary btn-sm" @click="removeCustomer(id)">Delete customer</button>
+        </li>
       </ul>
+      <form @submit.prevent="addCustomer">
+        <div>
+          <p>Add new customer</p>
+        </div>
+        <div class="form-group">
+          <label for="name">Name :</label>
+          <input type="text" class="form-control form-control-sm" id="name" v-model="newCustomer.name" />
+        </div>
+        <div class="form-group">
+          <label for="email">Email :</label>
+          <input type="email" class="form-control form-control-sm" id="email" v-model="newCustomer.email" />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
 </template>
 
@@ -15,17 +29,31 @@
 export default {
   data() {
     return {
+      newCustomer : {
+        id : '',
+        name : '',
+        email : '',
+      },
       customers : [
-        { name: 'Goran Jovicic' },
-        { name: 'Marko Trivunic' },
-        { name: 'Ivan Vujovic' }
+        { id: 1, name: 'Goran Jovicic', email: 'gugacns94@gmail.com' },
+        { id: 2, name: 'Marko Trivunic', email: 'marko@trivunic.com' },
+        { id: 3, name: 'Ivan Vujovic', email: 'ivan@vujovic.com' }
       ]
     }
   },
 
   methods : {
-    removeCustomer(index) {
-      this.customers.splice(index,1)
+    removeCustomer(id) {
+      this.customers.splice(id,1)
+    },
+
+    addCustomer() {
+      this.customers.push(this.newCustomer)
+      this.newCustomer = {
+        id : '',
+        name: '',
+        email: ''
+      }
     }
   }
 }
